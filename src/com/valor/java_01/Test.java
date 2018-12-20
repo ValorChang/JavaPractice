5b2d492478e69db10d3e02aed97d18e543b8fb81
valorchang <changdi92@gmail.com>
2018-12-21 01:23:15 +0800

    2018/12/21
    Signed-off-by: valorchang <changdi92@gmail.com>

--- src/com/valor/java_01/Test.java
+++ src/com/valor/java_01/Test.java
@@ -1,10 +1,15 @@
-package com.valor.java_01;
+ 		package com.valor.java_01;
+
+import java.util.Date;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ssss");
+		Date t = new Date();
+		System.out.println(t.getTime());
+		
+		
 	}
-
 }
